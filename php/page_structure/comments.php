5ff08c22cf2c7b7c34a5285b04a50f01b4ef0706
pickforbass <56023752+pickforbass@users.noreply.github.com>
2020-02-22 22:40:12 +0100

    22/02
    >DB : OK
    >Index.php : OK
    >Procedural system : OK
    >CSS : 60%
    
    ----- TO DO -----
    >Comments on files
    >Comments.php
    >Convert website in OOP
    >Login process & userpage

--- php/page_structure/comments.php
+++ php/page_structure/comments.php
@@ -3,15 +3,3 @@ $res = $conn->query("SELECT * from comment where article_article_id = $id");
 
 
 ?>
-
-
->DB : OK
->Index.php : OK
->Procedural system : OK
->CSS : 60%
-
------ TO DO -----
->Comments on files
->Comments.php
->Convert website in OOP
->Login process & userpage 
